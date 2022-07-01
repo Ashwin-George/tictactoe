aec76fc97faa1af093f058a6ed42b3f257c8dbc9
Ashwin-George <85630977+Ashwin-George@users.noreply.github.com>
2022-07-01 15:16:54 +0530

    Added SASS and styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Square = ({ value }) => {
   return (
     // eslint-disable-next-line react/button-has-type
-    <button type="button">{value}</button>
+    <button type="button" className="square">
+      {value}
+    </button>
   );
 };
 
