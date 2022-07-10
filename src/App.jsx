f5f73061690ee0f0d25c479b435a9f11e02c7b57
Ashwin-George <85630977+Ashwin-George@users.noreply.github.com>
2022-07-10 15:12:51 +0530

    gamining functionality & essentials implemented , styling remains

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import React from 'react';
 import Board from './components/Board';
+import History from './components/history';
+import StatusMessage from './components/StatusMessage';
+import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
 const App = () => {
+  const [history, setHistory] = useState(NEW_GAME);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  // console.log('history', history);
+
+  const { winner, winingSquares } = calculateWinner(current.board);
+
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
+      return;
+    }
+
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    if (move === 0) {
+      // setHistory({ board: Array(9).fill(null), isXNext: true });
+    }
+    setCurrentMove(move);
+  };
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <StatusMessage winner={winner} current={current} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winingSquares={winingSquares}
+      />
+      <button type="button" onClick={onNewGame}>
+        Start new Game
+      </button>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
   return (
     // eslint-disable-next-line react/button-has-type
-    <button type="button" className="square">
+    <button
+      type="button"
+      className="square"
+      onClick={onClick}
+      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
+    >
       {value}
     </button>
   );
